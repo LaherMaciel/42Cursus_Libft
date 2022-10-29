2c5f91b740d35ca0061e4317ff864189bf9aefa6
LaherMaciel <Laherwpayotmaciel@gmail.com>
2022-10-29 20:16:23 +0100

    #test

---
+++ ft_memcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/29 19:50:42 by lwencesl          #+#    #+#             */
+/*   Updated: 2022/10/29 19:58:12 by lwencesl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	*d = dest;
+	*s = src;
+	i = 0;
+	while (s[i] != '\0' && i <= n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (dest);
+}
