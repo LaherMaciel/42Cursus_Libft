add47be090ed89eb1bd0de8da9f6213e6882595a
LaherMaciel <Laherwpayotmaciel@gmail.com>
2022-11-07 21:41:51 +0000

    save

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,26 +6,28 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 18:35:01 by lwencesl          #+#    #+#             */
-/*   Updated: 2022/11/07 21:22:15 by lwencesl         ###   ########.fr       */
+/*   Updated: 2022/11/07 21:37:32 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/* 
+
 size_t	ft_in(char const *src, char const *set, size_t set_size)
 {
 	size_t	i;
 	size_t	j;
 	size_t	d;
+	size_t	w;
 
 	d = 0;
 	i = 0;
-	while (i < set_size)
+	while (src[i] != '\0')
 	{
 		j = 0;
+		w = d;
 		while (j < set_size)
 		{
-			if (set[j] == src[i])
+			if (src[i] == set[j])
 			{
 				d++;
 				break ;
@@ -33,6 +35,8 @@ size_t	ft_in(char const *src, char const *set, size_t set_size)
 			j++;
 		}
 		i++;
+		if (w == d)
+			break ;
 	}
 	return (d);
 }
@@ -43,27 +47,28 @@ size_t	ft_fim(char const *src, char const *set, size_t set_size)
 	size_t	j;
 	size_t	d;
 	size_t	g;
+	size_t	w;
 
-	i = 0;
+	i = -1;
 	g = ft_strlen(src);
 	d = 0;
-	while (i <= set_size)
+	while (src[++i] != '\0')
 	{
-		j = 0;
-		while (j < set_size)
+		j = -1;
+		w = d;
+		while (++j < set_size)
 		{
 			if (src[g - i] == set[j])
 			{
 				d++;
 				break ;
 			}
-			j++;
 		}
-		i++;
+		if (w == d)
+			break ;
 	}
 	return (g - d);
 }
- */
 
 char	*ft_strtrim(char const *src, char const *set)
 {
@@ -72,10 +77,8 @@ char	*ft_strtrim(char const *src, char const *set)
 	size_t	s;
 	char	*trim;
 
-	/*
 	j = ft_in(src, set, ft_strlen(set));
-	s = ft_fim(src, set, ft_strlen(set)) - j; 
-	*/
+	s = ft_fim(src, set, ft_strlen(set)) - j;
 	trim = (char *) malloc(s * sizeof(char) + 1);
 	if (!trim)
 		return (NULL);
