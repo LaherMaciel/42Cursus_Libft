9ce53be70b4ff34e4b82e160c30cc087511f074c
LaherMaciel <Laherwpayotmaciel@gmail.com>
2022-11-18 00:51:43 +0000

    Done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lwencesl <lwencesl@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 21:35:04 by lwencesl          #+#    #+#             */
-/*   Updated: 2022/11/16 21:35:04 by lwencesl         ###   ########.fr       */
+/*   Updated: 2022/11/18 00:48:31 by lwencesl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*temp;
+	t_list	*new;
+
+	new = NULL;
+	while (lst)
+	{
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, temp);
+		lst = lst->next;
+	}
+	return (new);
 }
